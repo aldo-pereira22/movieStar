ff4f6785d142ff46cb28374e869daa286ec592d5
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-04-21 09:32:13 -0300

    index.php
    Estilizando a página home, mostrando os filmes cadastrados.

--- index.php
+++ index.php
@@ -7,8 +7,38 @@
 ?>
 
     <div id="main-container" class="container-fluid"> 
-        <h1> Corpo do Site</h1>
-     </div>
+
+        <h2 class="section-title"> Filme Novos</h2>   
+        <p class="section-description">Veja as críticas dos últimos filmes adicionados no movieStar</p>
+        <div class="movies-container">
+            <div class="card movie-card">
+                <div class="card-img-top" style="background-image:url('<?= $BASE_URL?>'img/movies/movie_cover.jpg)"> </div>
+                <div class="card-body">
+                    <p class="card-rating">
+                        <i class="fa fa-star"></i>
+                        <span rating="">9</span>
+                    </p>
+                    <h5 class="card-title"> <a href="#"> Título do Filme</a> </h5>
+                    <a href="#" class="btn btn-primary rate-btn"> Avaliar</a>
+                    <a href="#" class="btn btn-primary card-btn"> Conhecer</a>
+
+                </div>
+            </div>
+        </div>
+
+        <h2 class="section-title"> Ação</h2>   
+        <p class="section-description">Veja os melhores filmes de Ação!</p>
+        <div class="movies-container">
+        
+        </div>
+
+        <h2 class="section-title"> Comédia</h2>   
+        <p class="section-description">Veja os melhores filmes de comédia!</p>
+        <div class="movies-container">
+        
+        </div>
+
+    </div>
 
 
 <?php
